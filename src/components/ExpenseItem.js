5a7cc8e1bf7ddf85908c78ba84c7b5477d863450
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-05-04 10:09:12 -0400

    first second

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,9 +3,7 @@ import './ExpenseItem.css';
 function ExpenseItem() {
   return (
     <div className='expense-item'>
-      <div>
-        March 28th 2021https://github.com/michaeljav/react-complete-guide.git
-      </div>
+      <div>March 28th 2021</div>
       <div className='expense-item__description'>
         <h2>Car Insurance</h2>
         <div className='expense-item__price'>$294.67</div>
