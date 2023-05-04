3708babace4f97a91ae30b7a5527ded5f8cf2e55
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-05-04 10:29:46 -0400

    Nuevos cambios  to main

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,7 @@ import './ExpenseItem.css';
 function ExpenseItem() {
   return (
     <div className='expense-item'>
-      <div>March 28th 2021</div>
+      <div>March 28th 2021 New changes</div>
       <div className='expense-item__description'>
         <h2>Car Insurance</h2>
         <div className='expense-item__price'>$294.67</div>
