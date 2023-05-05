361d5c29336148debed0f3a1a77ebd645af4c9f9
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-05-05 15:25:22 -0400

    58. Child-to-Parent Compt Commnication (Bottom-up)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 import './ExpenseForm.css';
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
@@ -57,14 +57,15 @@ const ExpenseForm = () => {
 
   const submitHandler = (event) => {
     event.preventDefault(); //prevent send data and reload page
-    console.log(enteredDate);
+    // console.log(enteredDate);
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
       date: new Date(enteredDate),
       /*date: enteredDate,*/
     };
-    console.log(expenseData);
+    //console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle('');
     setEnteredAmount('');
     setEnteredDate('');
