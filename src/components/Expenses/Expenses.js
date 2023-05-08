55d0b943663e8aaace4bd17f4ff82365a900eea2
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-05-08 12:33:08 -0400

    65. Understanding "Keys"

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -26,6 +26,7 @@ const Expenses = (props) => {
 
         {props.items.map((expense) => (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
