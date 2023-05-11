aed1d7359373bb9ac1953047b07083a7e5eef840
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-05-10 21:57:04 -0400

    77. Styled Components & Dynamic Props option 1

--- Project2_Todo/src/components/CourseGoals/CourseInput/CourseInput.js
+++ Project2_Todo/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,40 @@
 import React, { useState } from 'react';
-
+import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+const FromControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid input {
+    border-color: red;
+    background-color: red;
+  }
+  &.invalid label {
+    color: red;
+  }
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -24,10 +57,12 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      {/* <div className={`form-control ${!isValid ? 'invalid' : ''}`}> */}
+      <FromControl className={!isValid && 'invalid'}>
         <label> Course Goal</label>
         <input type='text' onChange={goalInputChangeHandler} />
-      </div>
+        {/* </div> */}
+      </FromControl>
       <Button type='submit'>Add Goal</Button>
     </form>
   );
