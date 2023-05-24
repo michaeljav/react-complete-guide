25b5d11a67b85f2f798495a19a08d6637d697289
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-05-23 20:37:25 -0400

    Error : This code is for creating a loop  render

--- Proj10_SideEffectsHooks/src/App.js
+++ Proj10_SideEffectsHooks/src/App.js
@@ -7,9 +7,15 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+  if (storedUserLoggedInInformation === '1') {
+    setIsLoggedIn(true);
+  }
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
