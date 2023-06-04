c88c917f9230a55f53b1625015f40c7b4f7b2fc2
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-06-04 18:46:17 -0400

    124. Building & Using a Custom Context Provider Component

--- Proj10_SideEffectsHooks/src/store/auth-context.js
+++ Proj10_SideEffectsHooks/src/store/auth-context.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
 
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  //corre la primera vez
+  //pero como en el segundo parametro tiene un array vacio y no tiene
+  //ninguna variable que haya cambiado, esta funcion hook useEffect correras
+  //solo la primera vez.
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 export default AuthContext;
